a0bcfaae1efe188dcd19ae6daeb3c05e7ff92f16
Kredwi <kredwicraft@gmail.com>
2026-01-10 02:07:04 +0900

    Reformat code and compression

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 const rightScrollButton = document.getElementById("right-button");
 const leftScrollButton = document.getElementById("left-button");
-const components = document.querySelector('.components');
+const components = document.querySelector(".components");
 
 rightScrollButton.addEventListener("click", () => {
-    components.scrollBy({
-        left: 300,
-        behavior: "smooth"
-    });
+  components.scrollBy({
+    left: 300,
+    behavior: "smooth",
+  });
 });
 
 leftScrollButton.addEventListener("click", () => {
-    components.scrollBy({
-        left: -300,
-        behavior: "smooth"
-    });
+  components.scrollBy({
+    left: -300,
+    behavior: "smooth",
+  });
 });
 
 const burderMenu = document.getElementById("burder-menu");
 function toggleBurgerMenu() {
-    if ( window.getComputedStyle(burderMenu).display == "none")
-        burderMenu.style.display = "block";
-    else burderMenu.style.display = "none";
+  if (window.getComputedStyle(burderMenu).display == "none")
+    burderMenu.style.display = "block";
+  else burderMenu.style.display = "none";
 }
