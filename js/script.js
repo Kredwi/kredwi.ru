62a96884b8d2464185c0515357b6e5ca77935c5d
Kredwi <kredwicraft@gmail.com>
2026-01-09 23:53:31 +0900

    Personal website

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const rightScrollButton = document.getElementById("right-button");
+const leftScrollButton = document.getElementById("left-button");
+const components = document.querySelector('.components');
+
+rightScrollButton.addEventListener("click", () => {
+    components.scrollBy({
+        left: 300,
+        behavior: "smooth"
+    });
+});
+
+leftScrollButton.addEventListener("click", () => {
+    components.scrollBy({
+        left: -300,
+        behavior: "smooth"
+    });
+});
+
+const burderMenu = document.getElementById("burder-menu");
+function toggleBurgerMenu() {
+    if ( window.getComputedStyle(burderMenu).display == "none")
+        burderMenu.style.display = "block";
+    else burderMenu.style.display = "none";
+}
